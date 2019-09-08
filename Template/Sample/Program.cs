becaa6326c44127e5a063d524c203800773d2295
haruanri <nanteneus@hotmail.co.jp>
2019-09-08 19:17:48 +0900

    add priority queue

--- Template/Sample/Program.cs
+++ Template/Sample/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using static Sample.Settings;
 using static System.Math;
 
 namespace Sample
@@ -13,9 +15,24 @@ namespace Sample
         {
             using (var sc = new SetConsole()) { Solve(); }
         }
-        private static void Solve()
+        public static void Solve()
         {
-            
+            var nm = GetArray<int>();
+            var jobs = new List<List<int>>(nm[1]);
+            for (int i = 0; i < nm[0]; i++)
+            {
+                var ab = GetArray<int>();
+                if (ab[0] > nm[1]) { continue; }
+                jobs[nm[1] - ab[0]].Add(ab[1]);
+            }
+
+            for (int i = nm[1] - 1; i >= 0; i--)
+            {
+                foreach (var j in jobs[i])
+                {
+
+                }
+            }
         }
     }
 
@@ -23,11 +40,34 @@ namespace Sample
     {
         protected static void CWrite<T>(T str) => Console.WriteLine(str);
         protected static string GetString() => Console.ReadLine();
+        protected static T GetNumber<T>()
+        {
+            var t = typeof(T);
+            if (t == typeof(int))
+            {
+                return (T)(object)GetString().ToInt();
+            }
+            else if (t == typeof(long))
+            {
+                return (T)(object)GetString().ToLong();
+            }
+            else if (t == typeof(double))
+            {
+                return (T)(object)GetString().ToDouble();
+            }
+            throw new NotSupportedException();
+        }
         protected static T[] GetArray<T>()
         {
             var str = Console.ReadLine();
             if (str == null) { throw new NullReferenceException("標準入力がnullです Solveメソッド内の解答コードが間違っています"); }
             return str.ToArray<T>();
+        }
+        protected static void Swap<T>(ref T item1, ref T item2)
+        {
+            var tmp = item1;
+            item1 = item2;
+            item2 = tmp;
         }
         protected class SetConsole : IDisposable
         {
@@ -47,6 +87,7 @@ namespace Sample
     }
     static class ExtentionsLibrary
     {
+        public static List<T> CopyList<T>(this List<T> list) => CopyArray(list.ToArray()).ToList();
         public static T[] CopyArray<T>(this T[] array)
         {
             var newArray = new T[array.Length];
@@ -92,5 +133,109 @@ namespace Sample
         public static double ToDouble(this string str) => double.Parse(str);
         public static BigInteger ToBigInteger(this string str) => BigInteger.Parse(str);
         public static DateTime ToDateTime(this string str) => DateTime.Parse(str);
+        public static string StringJoin(this object[] array, string separator) => string.Join(separator, array);
+    }
+    public class PriorityQueue<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private readonly Priority priority;
+        private List<T> heap = new List<T> { default };
+
+        private PriorityQueue() { }
+        public PriorityQueue(Priority p)
+        {
+            priority = p;
+        }
+        public int Count() => heap.Count - 1;
+        public T Peek()
+        {
+            if (heap.Count - 1 <= 0) { throw new Exception("要素が空です"); }
+            return heap[1];
+        }
+        public T Dequeue()
+        {
+            if (heap.Count - 1 <= 0) { throw new Exception("要素が空です"); }
+
+            var max = heap[1];
+            heap[1] = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+            MaxHeapify(1);
+            return max;
+        }
+        public void Enqueue(T key)
+        {
+            HeapIncreaseKey(key);
+        }
+        public void Clear()
+        {
+            heap = new List<T>() { default };
+        }
+        private void MaxHeapify(int i)
+        {
+            int largest;
+            int l = Left(i);
+            int r = Right(i);
+
+            if (l <= heap.Count - 1 && Compare(heap[i], heap[l]) < 0)
+            {
+                largest = l;
+            }
+            else
+            {
+                largest = i;
+            }
+            if (r <= heap.Count - 1 && Compare(heap[r] ,heap[largest]) > 0)
+            {
+                largest = r;
+            }
+
+            if (largest != i)
+            {
+                var temp = heap[i];
+                heap[i] = heap[largest];
+                heap[largest] = temp;
+                MaxHeapify(largest);
+            }
+        }
+        private void HeapIncreaseKey(T key)
+        {
+            heap.Add(key);
+            int lastIndex = heap.Count - 1;
+            while (lastIndex > 1 && Compare(heap[Parent(lastIndex)], heap[lastIndex]) < 0)
+            {
+                var temp = heap[lastIndex];
+                heap[lastIndex] = heap[Parent(lastIndex)];
+                heap[Parent(lastIndex)] = temp;
+                lastIndex = Parent(lastIndex);
+            }
+        }
+        private static int Parent(int i) => i / 2;
+        private static int Left(int i) => i * 2;
+        private static int Right(int i) => i * 2 + 1;
+        private int Compare(T x, T y)
+        {
+            return priority == Priority.Larger ? x.CompareTo(y) : y.CompareTo(x);
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for (int i = 1; i < heap.Count; i++)
+            {
+                yield return heap[i];
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 1; i < heap.Count; i++)
+            {
+                yield return heap[i];
+            }
+        }
+    }
+    public class Settings
+    {
+        public enum Priority : byte
+        {
+            Smaller = 0,
+            Larger = 1
+        }
     }
 }
--- Template/SampleTest/UnitTest1.cs
+++ Template/SampleTest/UnitTest1.cs
@@ -2,10 +2,13 @@ using System;
 using System.IO;
 using Xunit;
 using Sample;
+using System.Collections.Generic;
+using System.Linq;
+using static Sample.Settings;
 
 namespace SampleTest
 {
-    public class UnitTest 
+    public class ProblemsTest
     {
         [Fact]
         public void Test1()
@@ -25,7 +28,7 @@ namespace SampleTest
             AssertInOut(Input3, Output3);
         }
 
-        //[Fact]
+        [Fact]
         public void Test4()
         {
             AssertInOut(Input4, Output4);
@@ -54,5 +57,153 @@ namespace SampleTest
         internal static string Output2 { get; set; } = "Output2.txt";
         internal static string Output3 { get; set; } = "Output3.txt";
         internal static string Output4 { get; set; } = "Output4.txt";
+    }
+
+    public class LibraryTest
+    {
+        public class PriorityQueueTest : IDisposable
+        {
+            List<int> sourceList = new List<int> { 3, 5, 2, 1, 6, 7, 9, 8, 0, 4 };
+            readonly PriorityQueue<int> dscQue = new PriorityQueue<int>(Priority.Larger);
+            readonly PriorityQueue<int> ascQue = new PriorityQueue<int>(Priority.Smaller);
+
+            public PriorityQueueTest()
+            {
+                foreach (var item in sourceList)
+                {
+                    dscQue.Enqueue(item);
+                    ascQue.Enqueue(item);
+                }
+            }
+
+            [Fact]
+            public void 降順設定の場合中身を列挙するとき大きいもの順で出てくる()
+            {
+                var prev = int.MaxValue;
+                for (int i = 0; i < sourceList.Count; i++)
+                {
+                    Assert.True(prev > dscQue.Peek());
+                    prev = dscQue.Dequeue();
+                }
+            }
+
+            [Fact]
+            public void 昇順設定の場合中身を列挙するとき小さいもの順で出てくる()
+            {
+                var prev = int.MinValue;
+                for (int i = 0; i < sourceList.Count; i++)
+                {
+                    Assert.True(prev < ascQue.Peek());
+                    prev = ascQue.Dequeue();
+                }
+            }
+
+            [Fact]
+            public void 正しい要素数を返す()
+            {
+                Assert.Equal(sourceList.Count, dscQue.Count());
+            }
+
+            [Fact]
+            public void 要素を削除できる()
+            {
+                dscQue.Dequeue();
+                Assert.Equal(sourceList.Count - 1, dscQue.Count());
+            }
+
+            [Fact]
+            public void 降順設定の場合要素を削除するとき一番大きい値が削除される()
+            {
+                Assert.Equal(sourceList.Max(), dscQue.Dequeue());
+            }
+
+            [Fact]
+            public void 昇順設定の場合要素を削除するとき一番小さい値が削除される()
+            {
+                Assert.Equal(sourceList.Min(), ascQue.Dequeue());
+            }
+
+            [Fact]
+            public void 降順設定の場合要素を削除したときも大きいもの順で残りの要素が列挙される()
+            {
+                dscQue.Dequeue();
+                var prev = int.MaxValue;
+                for (int i = 0; i < dscQue.Count(); i++)
+                {
+                    Assert.True(prev > dscQue.Peek());
+                    prev = dscQue.Dequeue();
+                }
+            }
+
+            [Fact]
+            public void 昇順設定の場合要素を削除したときも小さいもの順で残りの要素が列挙される()
+            {
+                ascQue.Dequeue();
+                var prev = int.MinValue;
+                for (int i = 0; i < ascQue.Count(); i++)
+                {
+                    Assert.True(prev < ascQue.Peek());
+                    prev = ascQue.Dequeue();
+                }
+            }
+
+            [Fact]
+            public void 要素が0のとき取り出そうとするとエラー()
+            {
+                try
+                {
+                    for (int i = 0; i <= sourceList.Count; i++)
+                    {
+                        dscQue.Dequeue();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.Equal("要素が空です", ex.Message);
+                }
+                try
+                {
+                    dscQue.Peek();
+                }
+                catch (Exception ex)
+                {
+                    Assert.Equal("要素が空です", ex.Message);
+                }
+            }
+
+            [Fact]
+            public void LINQを使用できる()
+            {
+                var result = dscQue.Where(q => q < 5).OrderBy(o => o);
+                for (int i = 0; i < 5; i++)
+                {
+                    Assert.Equal(i, result.ElementAt(i));
+                }
+                Assert.Equal((0 + 4) * 5 / 2, result.Sum());
+            }
+
+            [Fact]
+            public void リストに変換できその中の要素も正しい()
+            {
+                Assert.Equal(typeof(List<int>), dscQue.ToList().GetType());
+                Assert.Equal(sourceList.Count, dscQue.ToList().Count);
+            }
+
+            [Fact]
+            public void 配列に変換できその中の要素も正しい()
+            {
+                Assert.Equal(typeof(int[]), dscQue.ToArray().GetType());
+                Assert.Equal(sourceList.Count, dscQue.ToList().Count);
+            }
+
+            [Fact]
+            public void クリアすると中身がなくなる()
+            {
+                ascQue.Clear();
+                Assert.Equal(0, ascQue.Count());
+            }
+
+            public void Dispose() { }
+        }
     }
 }
